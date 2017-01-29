f6a3d03b6ad1e1e55a536613c0567168f307ed41
Dan Lindsay <dlindsay72@hotmail.com>
2017-01-28 20:05:57 -0500

    Added spring bounce animation for custom button class

--- ForgetMeNot/CustomButton.swift
+++ ForgetMeNot/CustomButton.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 @IBDesignable
 
@@ -21,6 +22,46 @@ class CustomButton: UIButton {
     @IBInspectable var fontColor: UIColor = UIColor.white {
         didSet {
             self.tintColor = fontColor
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDown)
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDragEnter)
+        self.addTarget(self, action: #selector(CustomButton.scaleAnimation), for: .touchUpInside)
+        self.addTarget(self, action: #selector(CustomButton.scaleDefault), for: .touchDragExit)
+        
+    }
+    
+    func scaleToSmall() {
+        if let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY) {
+            scaleAnim.toValue = NSValue.init(cgSize: CGSize(width: 0.95, height: 0.95))
+            self.layer.pop_add(scaleAnim, forKey: "layerScaleSmallAnimation")
+        }
+    }
+    
+//    func scaleAnimation() {
+//        let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)!
+//        scaleAnim.velocity = NSValue.init(cgSize: CGSize(width: 10.0,height: 10.0))
+//        scaleAnim.toValue = NSValue.init(cgSize: CGSize(width: 1.0,height: 1.0))
+//        scaleAnim.springBounciness = 18
+//        self.layer.pop_add(scaleAnim, forKey: "layerScaleSpringAnimation")
+//    }
+    
+    func scaleAnimation() {
+        if let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY) {
+            scaleAnim.velocity = NSValue.init(cgSize: CGSize(width: 3.0, height: 3.0))
+            scaleAnim.toValue = NSValue.init(cgSize: CGSize(width: 1.0, height: 1.0))
+            scaleAnim.springBounciness = 18
+            self.layer.pop_add(scaleAnim, forKey: "layerScaleSpringAnimation")
+        }
+        
+    }
+    
+    func scaleDefault() {
+        if let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY) {
+            scaleAnim.toValue = NSValue.init(cgSize: CGSize(width: 1, height: 1))
+            self.layer.pop_add(scaleAnim, forKey: "layerScaleDefaultAnimation")
         }
     }
 }
