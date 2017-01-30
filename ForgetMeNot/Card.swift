f1ff88b468b19878632206a16479f3a34ed82b8b
Dan Lindsay <dlindsay72@hotmail.com>
2017-01-30 12:34:38 -0500

    created Card class with custom Card xib

---
+++ ForgetMeNot/Card.swift
@@ -0,0 +1,49 @@
+//
+//  Card.swift
+//  ForgetMeNot
+//
+//  Created by Dan Lindsay on 2017-01-30.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class Card: UIView {
+
+    let shapes = ["shape1", "shape2", "shape3"]
+    var currentshape: String!
+    
+    @IBOutlet weak var shapeImage: UIImageView!
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    override func awakeFromNib() {
+        setupView()
+        selectShape()
+    }
+    
+    func setupView() {
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowColor = UIColor(red: 157.0/255.0, green: 157.0/255.0, blue: 157.0/255.0, alpha: 1.0).cgColor
+        self.setNeedsLayout()
+    }
+    
+    func selectShape() {
+        currentshape = shapes[Int(arc4random_uniform(3))]
+        shapeImage.image = UIImage(named: currentshape)
+    }
+
+}
